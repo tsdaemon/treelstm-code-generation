2e23861fc48a070a4d5b1ccc4c4f96b05b7940aa
anatolii stehnii <tsdaemon@gmail.com>
2017-12-02 02:37:28 +0200

    ...

--- experiment_hs.py
+++ experiment_hs.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     # subprocess.run("python main.py -dataset hs -cuda -encoder bi-lstm -no_last_tanh "
     #                "-output_dir ./results/hs/bilstm_no_tanh", shell=True)
 
-    subprocess.run("python main.py -dataset hs -cuda"
-                   "-output_dir results/hs/ccg_recursive", shell=True)
+    subprocess.run("python main.py -dataset hs -cuda "
+                   "-output_dir /results/hs/ccg_recursive", shell=True)
