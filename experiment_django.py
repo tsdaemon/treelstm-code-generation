f94e52ed6e735fa71201400e82df01808c3cf7a3
Anatoliy Stegniy <tsdaemon@gmail.com>
2017-12-07 19:59:42 +0200

    ...

--- experiment_django.py
+++ experiment_django.py
@@ -3,11 +3,11 @@ import subprocess
 
 if __name__ == '__main__':
     # with unary closures
-    subprocess.run("python main.py -dataset django -cuda -encoder bi-lstm-dropout "
-                   "-output_dir ./results/django/unary_closures/bilstm "
-                   "-batch_size 50 "
-                   "-unary_closures "
-                   "-data_dir ./preprocessed/django", shell=True)
+    # subprocess.run("python main.py -dataset django -cuda -encoder bi-lstm-dropout "
+    #                "-output_dir ./results/django/unary_closures/bilstm "
+    #                "-batch_size 50 "
+    #                "-unary_closures "
+    #                "-data_dir ./preprocessed/django", shell=True)
 
     subprocess.run("python main.py -dataset django -cuda -syntax dependency "
                    "-output_dir ./results/django/unary_closures/dependency "
