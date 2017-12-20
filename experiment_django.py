51dc5b4d22b6ac697114a69c5cbe227071723385
Anatolii Stehnii <anatolii.stehnii@gmail.com>
2017-12-20 17:35:25 +0200

    ...

--- experiment_django.py
+++ experiment_django.py
@@ -36,13 +36,13 @@ if __name__ == '__main__':
     #                "-data_dir ./preprocessed/django", shell=True)
 
     # without unary closures
-    subprocess.run("python main.py -dataset django -cuda -syntax dependency "
-                   "-output_dir ./results/django/no_unary_closures/dependency "
-                   "-batch_size 50 "
-                   "-no_unary_closures "
-                   "-decode_max_time_step 100 "
-                   "-max_example_action_num 100 "
-                   "-data_dir ./preprocessed/django", shell=True)
+    # subprocess.run("python main.py -dataset django -cuda -syntax dependency "
+    #                "-output_dir ./results/django/no_unary_closures/dependency "
+    #                "-batch_size 50 "
+    #                "-no_unary_closures "
+    #                "-decode_max_time_step 100 "
+    #                "-max_example_action_num 100 "
+    #                "-data_dir ./preprocessed/django", shell=True)
 
     # subprocess.run("python main.py -dataset django -cuda -syntax pcfg "
     #                "-output_dir ./results/django/no_unary_closures/pcfg "
@@ -60,10 +60,10 @@ if __name__ == '__main__':
     #                "-max_example_action_num 100 "
     #                "-data_dir ./preprocessed/django", shell=True)
     #
-    # subprocess.run("python main.py -dataset django -cuda -encoder bi-lstm-dropout "
-    #                "-output_dir ./results/django/no_unary_closures/bilstm "
-    #                "-batch_size 50 "
-    #                "-no_unary_closures "
-    #                "-decode_max_time_step 100 "
-    #                "-max_example_action_num 100 "
-    #                "-data_dir ./preprocessed/django", shell=True)
+    subprocess.run("python main.py -dataset django -cuda -encoder bi-lstm-dropout "
+                   "-output_dir ./results/django/no_unary_closures/bilstm "
+                   "-batch_size 50 "
+                   "-no_unary_closures "
+                   "-decode_max_time_step 100 "
+                   "-max_example_action_num 100 "
+                   "-data_dir ./preprocessed/django", shell=True)
